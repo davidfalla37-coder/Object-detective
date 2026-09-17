f5b94ef3d2e8fe2f03af6ca84bbf322947d7f24f
davidfalla37-coder <davidfalla37@gmail.com>
2026-09-17 17:34:44 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/objectdetective/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.objectdetective.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
