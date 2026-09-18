33f54f1e222a326ce6c2e0d26009da9417bacbe2
davidfalla37-coder <davidfalla37@gmail.com>
2026-09-18 15:02:08 +0100

    Update MainActivity.kt

--- app/src/main/java/com/objectdetective/app/MainActivity.kt
+++ app/src/main/java/com/objectdetective/app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.objectdetective.app
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : Activity() {
+
+    private var fileCallback: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +22,42 @@ class MainActivity : Activity() {
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
-        webView.addJavascriptInterface(WebAppInterface(), "Android")
-        webView.loadUrl("file:///android_asset/index.html")
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                fileCallback?.onReceiveValue(null)
+                fileCallback = filePathCallback
+
+                val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                    
+             type = "image/*"
+addCategory(Intent.CATEGORY_OPENABLE)
+}
+
+startActivityForResult(intent, 1001)
+return true
+}
+}
+
+webView.loadUrl("file:///android_asset/index.html")
+}
+
+override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == 1001) {
+        val result = if (resultCode == RESULT_OK) {
+            data?.data?.let { arrayOf(it) }
+        } else {
+            null
+        }
+
+        fileCallback?.onReceiveValue(result)
+        fileCallback = null
     }
 }
+}       
