692f4c2760fbb9e9cfd4fc16e5a9b78fe6473bcc
davidfalla37-coder <davidfalla37@gmail.com>
2026-09-18 07:42:21 +0100

    Create WebAppInterface.kt

---
+++ app/src/main/java/com/objectdetective/app/WebAppInterface.kt
@@ -0,0 +1,12 @@
+package com.objectdetective.app
+
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+
+class WebAppInterface {
+
+    @JavascriptInterface
+    fun identifyObject() {
+        // This confirms the button can communicate with Android.
+    }
+}
